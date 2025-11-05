5a11d910cb16bcc954a01d4e2de96d9eadff714d
roryhe <aannnaa@foxmail.com>
2025-11-05 10:25:27 +0800

    [roryhe]-[update]-[task-tow 通道缓冲机制]

---
+++ go-record/study-base/task-two/channel-buffer.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+/*
+实现一个带有缓冲的通道，生产者协程向通道中发送100个整数，消费者协程从通道中接收这些整数并打印。
+考察点 ：通道的缓冲机制。
+*/
+
+func producer(ch chan int) {
+	for i := 0; i < 100; i++ {
+		ch <- i
+		fmt.Println("producer ", i)
+		time.Sleep(20 * time.Millisecond)
+	}
+	close(ch)
+}
+
+func consumer(ch chan int) {
+	for c := range ch {
+		fmt.Println("consumer ", c)
+		time.Sleep(10 * time.Millisecond)
+	}
+}
+
+func main() {
+	ch := make(chan int, 3)
+	go producer(ch)
+	go consumer(ch)
+	time.Sleep(5 * time.Second)
+	fmt.Println("main end")
+}
